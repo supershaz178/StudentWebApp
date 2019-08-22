ebcf35c496293908313605433080282ae979fbb2
supershaz178 <supershaz178@users.noreply.github.com>
2019-08-21 22:37:53 -0500

    Add Student repo to interact with database

---
+++ src/main/java/com/region20/student/app/dashboard/repository/StudentRepository.java
@@ -0,0 +1,31 @@
+package com.region20.student.app.dashboard.repository;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.region20.student.app.dashboard.persistence.Student;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Integer> {
+		
+	public List<Student> findBySchoolYears(List<Integer> years); 
+	
+	public List<Student> findByCampus(List<Integer> campuses); 
+	
+	public List<Student> findByExtStudentId(List<Integer> studentIds); 
+	
+	public List<Student> findEntryDate(List<Date> entryDates); 
+	
+	public List<Student> findByGradeLevel(List<Integer> gradeLevels); 
+	
+	public List<Student> findByFristName(List<String> firstName); 
+	
+	public List<Student> findByLastName(List<String> lastName); 
+	
+	public List<Student> findByFullName(String firstName, String lastName); 
+
+	public List<Student> findByFullNames(List<String> fullNames); 
+}
